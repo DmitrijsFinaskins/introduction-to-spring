037496192d26010fda9ff3e7df00b21e406de1b4
DmitrijsF <dmitrijs@jugla.net>
2022-06-10 16:35:02 +0300

    application properties in tests, @Value default value

--- src/test/java/io/proj3ct/springstarter/SpringStarterApplicationTests.java
+++ src/test/java/io/proj3ct/springstarter/SpringStarterApplicationTests.java
@@ -2,12 +2,25 @@ package io.proj3ct.springstarter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = io.proj3ct.springstarter.SpringConfig.class)
 class SpringStarterApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void valuesForTest(){
+		ApplicationContext ctx = new
+				AnnotationConfigApplicationContext(SpringConfig.class);
+
+		Car car = ctx.getBean("carWithModelValue" ,Car.class);
+
+		assertEquals("Audi", car.getManufacturer());
+	}
 }
